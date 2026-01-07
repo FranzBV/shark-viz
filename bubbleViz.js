1eb1dfbfdb15b151167361264ab8a65b31bbb91c
FranzBV <villaminfranz@gmail.com>
2026-01-07 11:28:48 -0700

    fix: imported correct version

--- bubbleViz.js
+++
@@ -1,6 +0,0 @@
-// bubbleViz.js - Bubble chart visualization
-// Add your loadAndPrepareData function here
-
-function loadAndPrepareData() {
-  console.log('Bubble chart - add your implementation');
-}
